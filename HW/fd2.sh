347b00f42e89a52f958b82ee4d72f5d86bd8bfdf
ivan19911502 <ivanshakov1999@gmail.ru>
2023-10-18 20:53:26 +0300

    added new file fd2.sh

--- HW/fd2.sh
+++ HW/fd2.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash 
 echo "This is a standard output that will be silenced" > /dev/null
-echo "This is an error message that will be silenced" > /dev/null 2>&1 
+echo "This is an error message that will be silenced" 2> /dev/null 
 exit 0 
