fd0a7d88a769c7743feb631e0694a6ca9a08f053
ivan19911502 <ivanshakov1999@gmail.ru>
2023-10-21 22:35:20 +0300

    new file arg.sh

--- HW/arg.sh
+++ HW/arg.sh
@@ -3,7 +3,7 @@
 if [ -n "$1" ]; then
 
 
-if [[ $(wc -c $1 | awk '{print $1}') -le 1024 ]]; then 
+if [ $(wc -c "$1" | awk '{print $1}') -le 1024 ]; then 
 	echo "Expected Output: OK"
 else
 	echo "FAIL"
