564d1971d8eeaac3d5657d489102ca76136d1639
ivan19911502 <ivanshakov1999@gmail.ru>
2023-10-22 23:37:23 +0300

    added new file

--- HW/listScript.sh
+++ HW/listScript.sh
@@ -2,13 +2,13 @@
 
 if [[ $1 -le 32 ]]; then
 
-	alfabet=(abcdefghijklmnopqrstuvwxyz)
+	alfabet=("abcdefghijklmnopqrstuvwxyz")
 
 
 
 	alfa=${alfabet:0:$1}
 	for (( i=0; i<${#alfa}; i++ )); do
-		mkdir dir_${alfabet:$i:1}
+		mkdir dir_${alfa:$i:1}
 	done
 else
 	echo "Error"
