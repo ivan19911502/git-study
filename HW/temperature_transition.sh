a27e78fde9cfc5ca526bd447af53c8a6ee001c7e
ivan19911502 <ivanshakov1999@gmail.ru>
2023-10-25 00:03:34 +0300

    added new file

--- HW/temperature_transition.sh
+++ HW/temperature_transition.sh
@@ -7,13 +7,14 @@ cifra=${#string}-1
 a=${string:(-1):1}
 b=${string:0:$cifra}
 
-	if [ "$a" = "C" ] && [ $b -ge 0 ]; then
-	result=$((x + $b))K
-	echo $result
-	elif [ "$a" = "K" ] && [ $b -ge 273 ]; then
-	result1=$(($b - x))C
-	echo $result1
-	else
-	echo "Error"
-	fi
+        if [ "$a" = "C" ] && [ $b -ge 0 ]; then
+        result=$((x + $b))K
+        echo $result
+        elif [ "$a" = "K" ] && [ $b -ge 273 ]; then
+        result1=$(($b - x))C
+        echo $result1
+        else
+        echo "Error"
+        fi
+
 
