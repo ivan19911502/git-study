f95b0b7c19990a98323beb9fc5cc8d38406661b1
ivan19911502 <ivanshakov1999@gmail.ru>
2023-11-19 19:15:38 +0300

    /Python

---
+++ Python/1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+
+a = int(input("Plese input age:"))
+
+
+if a < 18:
+    print("You are a child.")
+elif a == 18:
+    print("You are a teenager.")
+else:
+    print("You are an adult.")
