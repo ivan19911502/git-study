a8941ea75e290b0dd0564d4b4a3b5356a4736f4f
Ivan Shakov <jcuke@MacBook-Pro-jcuke.local>
2024-05-26 16:34:33 +0300

    5

--- variables.tf
+++ variables.tf
@@ -14,3 +14,69 @@ variable "unique_ids" {
   type    = list(string)
   default = ["UID123, UID456, UID789"]
 }
+
+
+variable "cloud_users_list" {
+  type = list
+  default = [
+    "andrew", "ken", "faraz", "mutsumi", "peter", "steve", "braja"
+  ]
+}
+
+
+variable "media" {
+  type = list
+  default = [ 
+    "/media/tails.jpg",
+    "/media/eggman.jpg",
+    "/media/ultrasonic.jpg",
+    "/media/knuckles.jpg",
+    "/media/shadow.jpg",
+  ]
+}
+
+variable "sf_subset" {
+  type = string
+  default = (
+    "ryu, ken, akuma, seth, zangief, poison, gen, oni, thawk, fang, rashid, birdie, sagat, bison, cammy, chun-li, balrog, cody, rolento, ibuki"
+  )
+}
+
+
+variable "sf_map" {
+  type = map
+  default = {
+    "ryu" = "true", 
+    "ken" = "true", 
+    "akuma" = "true",
+    "seth" = "true", 
+    "zangief" = "true",
+    "poison" = "true",
+    "gen" = "true",
+    "oni" = "true", 
+    "thawk" = "true", 
+    "fang" = "true",
+    "rashid" = "true", 
+    "birdie" = "true", 
+    "sagat" = "true", 
+    "bison" = "true", 
+    "cammy" = "true",
+    "chun-li" = "true", 
+    "balrog" = "true", 
+    "cody" = "true", 
+    "rolento" = "true", 
+    "ibuki" = "false"
+  }
+  description = "record"
+}
+
+
+variable "reversed_sf" {
+  type = list(string)
+  default = [
+    "ryu", "ken", "akuma", "seth", "zangief",
+    "poison", "gen", "oni", "thawk", "fang",
+    "rashid", "birdie", "sagat", "bison", "cammy",
+    "chun-li", "balrog", "cody", "rolento", "ibuki"
+  ]
+}
