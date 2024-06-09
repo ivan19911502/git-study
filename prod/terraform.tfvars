3cfe1e7c2d68fc7909d2ba7c28494ab556b2c6d2
jcuke <ivanshakov19911502@mail.ru>
2024-06-09 19:45:32 +0300

    10

---
+++ prod/terraform.tfvars
@@ -0,0 +1,5 @@
+server_ip = "10.10.1.1"
+server_port = "8082"
+server_name = "prod"
+filename = "./prod.txt"
+file_permission = 0700
---
+++ prod/variables.tf
@@ -0,0 +1,22 @@
+variable "filename" {
+  type        = string
+  description = "name file config"
+}
+
+
+
+variable "file_permission" {
+  type = number
+}
+
+variable "server_ip" {
+  type = string
+}
+
+variable "server_port" {
+  type = string
+}
+
+variable "server_name" {
+  type = string
+}
